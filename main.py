a24322484fe483e36f42886311f7135f7402f2b6
sviatoslav-shubnikov <slavik20012008@gmail.com>
2024-12-04 16:09:12 +0300

    json+html

--- main.py
+++ main.py
@@ -6,13 +6,100 @@ url = "https://github.com/trending"
 
 response = requests.get(url)
 soup = bs(response.text, "html.parser")
+count = 1
+
 for data in soup.find_all('article', class_="Box-row"):
     
-    repo = data.find('a', class_='Link').get_text()
+    repo = data.find('a', class_='Link').get_text().strip().replace(' ', '').splitlines()
     
-    stars = data.find('a', class_='Link Link--muted d-inline-block mr-3').get_text()
-    datas.append({'repos': repo, "stars": stars})
+    stars = data.find('a', class_='Link Link--muted d-inline-block mr-3').get_text().strip().replace(' ', '')
+    repos = repo[0]+repo[2]
+    datas.append({'Repository': repos, "Stars": stars})
 
-    print(f"repo: {repo}, stars: {stars}")
+    print(f"{count}. Repository: {repos}; Stars: {stars};")
 
+    count+=1
+
+# def save_to_json(datas, file="data.json"):
+#     with open(file,"w",encoding="utf-8")as f:
+#         json.dump(datas, f, ensure_ascii=False, indent=4)
+
+# save_to_json(datas)
+
+def html(file="data.json", file_html="index.html"):
+    with open(file,"r",encoding="utf-8")as f:
+        datas=json.load(f)
     
+    html_cont='''
+<html>
+    <head>
+        <title>Список репозиториев</title>
+        <style>
+            body {
+                color: #333;
+                background-color: #f4f4f4;
+            }
+            table {
+                background: #ffffff;
+            }
+            th, td {
+                padding: 15px;
+                border: 1px solid #ccc;
+                text-align: center;
+            }
+            th {
+                background-color: #e0e0e0;
+                color: #000;
+            }
+            h1 {
+                text-align: left;
+                margin-bottom: 20px;
+            }
+            p {
+                text-align: left;
+                margin-top: 20px;
+            }
+            a {
+                color: #00ff62;
+                text-decoration: underline;
+            }
+        </style>
+    </head>
+    <body>
+        <div>
+            <h1>Коллекция репозиториев</h1>
+            <table>
+                <tr>
+                    <th>№</th>
+                    <th>Repository</th>
+                    <th>Stars</th>
+                </tr>
+    '''
+
+    # Добавление цитат в таблицу
+    for id, data in enumerate(datas, 1):
+        html_cont += f'''
+        <tr>
+            <td><h3>{id}</h3></td>
+            <td><h3>{data["Repository"]}</h3></td>
+            <td><h3>{data["Stars"]}<h3></td>
+        </tr>
+        '''
+
+    # Завершение HTML-контента
+    html_cont += '''
+            </table>
+            <p>
+                <h2><a href="https://github.com/trending">Источник: Repositories in Trend (GitHUB)</a></h2>
+            </p>
+        </div>
+    </body>
+</html>
+    '''
+
+    # Запись HTML-контента в файл
+    with open(file_html, "w", encoding="utf-8") as f:
+        f.write(html_cont)
+
+# Вызов функции для генерации HTML
+html()
